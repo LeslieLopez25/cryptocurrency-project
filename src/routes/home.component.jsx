62b29f1673ced5546ad842d740b6bf750253b902
Leslie Lopez <leslie2515lopez@gmail.com>
2023-02-26 23:54:45 -0600

    Trending coins component

--- src/routes/home.component.jsx
+++ src/routes/home.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import CoinSearch from "../components/coinSearch";
+import Trending from "../components/trending";
 
 const Home = ({ coins }) => {
   return (
     <div>
       <CoinSearch coins={coins} />
+      <Trending />
     </div>
   );
 };
