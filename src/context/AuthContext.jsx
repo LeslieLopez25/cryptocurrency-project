a3f705a572dd848601006e823b8df8f9d63f098c
Leslie Lopez <leslie2515lopez@gmail.com>
2023-08-03 23:14:20 -0600

    Updated authcontext and sign in component

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -20,7 +20,6 @@ export const AuthContextProvider = ({ children }) => {
       watchList: [],
     });
   };
-
   const signIn = (email, password) => {
     return signInWithEmailAndPassword(auth, email, password);
   };
