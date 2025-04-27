d269c7dad737cb7238245a5844f0656febd26d05
Leslie Lopez <leslie2515lopez@gmail.com>
2025-04-26 23:56:20 -0600

    Added comments to navigation component

--- src/components/navbar.component.jsx
+++ src/components/navbar.component.jsx
@@ -10,10 +10,12 @@ const Navbar = () => {
   const { user, logout } = UserAuth();
   const navigate = useNavigate();
 
+  // Toggles mobile navigation menu open/close
   const handleNav = () => {
     setNav(!nav);
   };
 
+  // Logs out the user and redirects to the home page
   const handleSignOut = async () => {
     try {
       await logout();
@@ -25,13 +27,16 @@ const Navbar = () => {
 
   return (
     <div className="rounded-div flex items-center justify-between h-20 font-bold">
+      {/* Logo / Home link */}
       <Link to="/">
         <h1 className="text-2xl">Cryptobase</h1>
       </Link>
+      {/* Desktop Theme Toggle */}
       <div className="hidden md:block">
         <ThemeToggle />
       </div>
 
+      {/* Desktop Navigation Links */}
       {user?.email ? (
         <div>
           <Link to="/account" className="p-4">
@@ -55,12 +60,12 @@ const Navbar = () => {
         </div>
       )}
 
-      {/* Menu Icon */}
+      {/* Mobile Menu Toggle Icon */}
       <div onClick={handleNav} className="block md:hidden cursor-pointer z-10">
         {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={20} />}
       </div>
 
-      {/* Mobile Menu */}
+      {/* Mobile Navigation Menu */}
       <div
         className={
           nav
@@ -79,6 +84,7 @@ const Navbar = () => {
             <ThemeToggle />
           </li>
         </ul>
+        {/* Mobile Auth Buttons */}
         <div className="flex flex-col w-full p-4">
           <Link to={"/signin"}>
             <button
