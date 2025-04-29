b1ccdb93a9ed68df1c46673c482e0766856bd28a
Leslie Lopez <leslie2515lopez@gmail.com>
2025-04-28 23:57:21 -0600

    Added comments to home component

--- src/routes/home.component.jsx
+++ src/routes/home.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import CoinSearch from "../components/coin-search.component";
 import Trending from "../components/trending.component";
 
+// Home component displays the CoinSearch and Trending sections
 const Home = ({ coins }) => {
   return (
     <div>
