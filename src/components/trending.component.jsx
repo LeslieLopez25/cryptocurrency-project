0438cc2b8a608fb6babf2a6ad8d89c2b2d057eaf
Leslie Lopez <leslie2515lopez@gmail.com>
2025-04-28 22:40:05 -0600

    Added comments to trending component

--- src/components/trending.component.jsx
+++ src/components/trending.component.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+// Fetches trending coins from CoinGecko and displays them in a styled grid
 const Trending = () => {
   const [trending, setTrending] = useState([]);
 
