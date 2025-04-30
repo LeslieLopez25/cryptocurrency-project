001261d1f841d4643e44371b3deb18e0a9b5e010
Leslie Lopez <leslie2515lopez@gmail.com>
2025-04-29 23:57:58 -0600

    Added comments to app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React, { lazy, Suspense, useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar.component";
 import { ThemeProvider } from "./context/ThemeContext";
-import Home from "./routes/home.component";
-import Signin from "./routes/sign-in.component";
-import Signup from "./routes/sign-up.component";
-import Account from "./routes/account.component";
-import CoinPage from "./routes/coin-page.component";
 import axios from "axios";
 import Footer from "./components/footer.component";
 import { AuthContextProvider } from "./context/AuthContext";
 
+// Lazily import route components to optimize performance
+const Home = lazy(() => import("./routes/home.component"));
+const Signin = lazy(() => import("./routes/sign-in.component"));
+const Signup = lazy(() => import("./routes/sign-up.component"));
+const Account = lazy(() => import("./routes/account.component"));
+const CoinPage = lazy(() => import("./routes/coin-page.component"));
+
 function App() {
   const [coins, setCoins] = useState([]);
 
+  // URL for fetching top 10 cryptocurrency in MXN
   const url =
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=mxn&order=market_cap_desc&per_page=10&page=1&sparkline=true";
 
+  // Fetch crypto market data when components mounts
   useEffect(() => {
     axios.get(url).then((response) => {
       setCoins(response.data);
@@ -24,18 +28,22 @@ function App() {
   }, [url]);
 
   return (
+    // Provide theme context to all children components
     <ThemeProvider>
       <AuthContextProvider>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home coins={coins} />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/account" element={<Account />} />
-          <Route path="/coin/:coinId" element={<CoinPage />}>
+        <Suspense
+          fallback={<div className="text-center py-10">Loading...</div>}
+        >
+          <Routes>
+            <Route path="/" element={<Home coins={coins} />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/coin/:coinId" element={<CoinPage />} />
             <Route path=":coinId" />
-          </Route>
-        </Routes>
+          </Routes>
+        </Suspense>
         <Footer />
       </AuthContextProvider>
     </ThemeProvider>
